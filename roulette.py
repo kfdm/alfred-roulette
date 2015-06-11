e5cd455f093aac070446ccb7662c32195d1931ea
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-06-12 07:56:48 +0900

    Add user/repo to display

--- roulette.py
+++ roulette.py
@@ -4,15 +4,18 @@ import random
 from workflow import Workflow, web
 
 
-def get_web_data():
+def open_issues():
     return web.get('https://api.github.com/search/issues?q=is:open%20is:issue%20user:kfdm').json()
+
 
 def main(wf):
     # Save data from `get_web_data` for 30 seconds under
     # the key ``example``
-    data = wf.cached_data('example', get_web_data, max_age=30)
+    data = wf.cached_data('github_issues', open_issues, max_age=30)
     for datum in random.sample(data['items'], 4):
-        wf.add_item(datum['title'], datum['body'], arg=datum['html_url'], valid=True)
+        parts = datum['html_url'].split('/')
+        title = '{0}/{1} {2}'.format(parts[3], parts[4], datum['title'])
+        wf.add_item(title, datum['body'], arg=datum['html_url'], valid=True)
     wf.send_feedback()
 
 if __name__ == '__main__':

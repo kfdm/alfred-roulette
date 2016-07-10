18631e64c2cc6db8bf6ec65983a34c7ddcd34a35
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-07-10 20:32:26 +0900

    Default to 10 minute cache

--- roulette.py
+++ roulette.py
@@ -5,13 +5,13 @@ from workflow import Workflow, web
 
 
 def open_issues():
-    return web.get('https://api.github.com/search/issues?q=is:open%20is:issue%20user:kfdm').json()
+    return web.get('https://api.github.com/search/issues?q=is:open%20is:issue%20user:kfdm&per_page=100').json()
 
 
 def main(wf):
-    # Save data from `get_web_data` for 30 seconds under
-    # the key ``example``
-    data = wf.cached_data('github_issues', open_issues, max_age=30)
+    # Cache our list of issues since it's unlikely to change much during the
+    # time when we're calling this script
+    data = wf.cached_data('github_issues', open_issues, max_age=600)
     for datum in random.sample(data['items'], 4):
         parts = datum['html_url'].split('/')
         title = '{0}/{1} {2}'.format(parts[3], parts[4], datum['title'])
